993a83884d0a3be0b20c0beb2cc845b2d10f5d06
Salman Ahmad <salmanahmad111499@gmail.com>
2025-09-30 22:12:05 +0100

    just initialized the repository

---
+++ subscription_manager/subscription.py
@@ -0,0 +1,11 @@
+# This file contains the subscription class
+
+
+class subscription:
+    """This class is responsible for hanling an excel file in a certain pattern.
+    It reads the excel file and performs various tasks such as updating the next bill month, notifying the admin for his upcoming subscriptions e.t.c
+    """
+    
+    # constructor
+    def __init__(self):
+        pass
